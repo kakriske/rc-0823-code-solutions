95ee62d5faba5343f09cccbdc81f414cb15bd8fe
Kyle Kriske <kralex8.617@gmail.com>
2023-08-29 18:40:19 +0000

    completed javascript-arrays

---
+++ javascript-arrays/main.js
@@ -0,0 +1,16 @@
+const colors = ['red', 'white', 'blue'];
+console.log('value of colors[0]:', colors[0]);
+console.log('value of colors[1]:', colors[1]);
+console.log('value of colors[2]:', colors[2]);
+console.log(
+  'America is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2] + '.'
+);
+colors[2] = 'green';
+console.log('Mexico is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2]);
+console.log(colors);
+const students = ['Peter', 'Gus', 'Sam', 'Lee'];
+const numberOfStudents = students.length;
+console.log(numberOfStudents);
+const lastIndex = students[3];
+console.log(lastIndex);
+console.log(students);
