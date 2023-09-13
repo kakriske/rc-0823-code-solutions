90aaea81a2064a328656034fc833cb25ffc61605
Kyle Kriske <kralex8.617@gmail.com>
2023-09-13 19:22:49 +0000

    javascript-forms

---
+++ javascript-forms/form-events.js
@@ -0,0 +1,30 @@
+function handleFocus(event) {
+  console.log('focus event was fired');
+  console.log('event.target.name', event.target.name);
+}
+
+function handleBlur(event) {
+  console.log('blur event was fired');
+  console.log('event.target.name', event.target.name);
+}
+
+function handleInput(event) {
+  console.log('Value of', event.target.name + ':', event.target.value);
+  console.log('input:', event.target.value);
+}
+
+const $nameIn = document.querySelector('#user-name');
+const $emailIn = document.querySelector('#user-email');
+const $messageIn = document.querySelector('#user-message');
+
+$nameIn.addEventListener('focus', handleFocus);
+$emailIn.addEventListener('focus', handleFocus);
+$messageIn.addEventListener('focus', handleFocus);
+
+$nameIn.addEventListener('blur', handleBlur);
+$emailIn.addEventListener('blur', handleBlur);
+$messageIn.addEventListener('blur', handleBlur);
+
+$nameIn.addEventListener('input', handleInput);
+$emailIn.addEventListener('input', handleInput);
+$messageIn.addEventListener('input', handleInput);
